2a39f5a45d527bffa6f0cfb8494011837f24f4c5
priyasharma2323 <140616287+priyasharma2323@users.noreply.github.com>
2024-05-04 23:15:03 +0530

    Add files via upload

---
+++ day1 (1).py
@@ -0,0 +1,8 @@
+# creating a basic calculator to perform basic arithmetic operations on 2 numbers
+a = 50
+b = 3
+
+print("The value of", a, "+", 3, "is: ", a + b)
+print("The value of", a, "-", 3, "is: ", a - b)
+print("The value of", a, "*", 3, "is: ", a * b)
+print("The value of", a, "/", 3, "is: ", a / b)
